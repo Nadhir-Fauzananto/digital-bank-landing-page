50e906cc951113e9311d9b64718087c55f1920f6
M. Nadhir Fauzananto <nadhir.fauzananto@gmail.com>
2026-06-16 09:15:28 +0200

    add lookups guard against missing DOM element in script.js

--- script.js
+++ script.js
@@ -4,36 +4,49 @@ const navLinks = document.getElementById("nav-links")
 const navOverlay= document.getElementById("nav-overlay")
 const navigationLinkItems = document.querySelectorAll(".navigation-link")
 
-navigationLinkItems.forEach(link => {
-    link.addEventListener("click", () => {
-        document.querySelector(".navigation-link.active-page")?.classList.remove("active-page")
-        link.classList.add("active-page")
+if (navigationLinkItems.length > 0) {
+    navigationLinkItems.forEach(link => {
+        link.addEventListener("click", () => {
+            document.querySelector(".navigation-link.active-page")?.classList.remove("active-page")
+            link.classList.add("active-page")
+        })
     })
-})
+} else {
+    console.error("navigationLinkItems does not exist")
+}
+
 
 // open menu
 function openMenu() {
+    if (openMenuBtn && closeMenuBtn && navLinks && navOverlay) {
     navLinks.classList.remove("hidden")
     closeMenuBtn.classList.remove("hidden")
     openMenuBtn.classList.add("hidden")
     navOverlay.classList.remove("hidden")
     navOverlay.classList.remove("overlay-hidden")
     navOverlay.classList.add("overlay-active")
-    openMenuBtn.ariaExpanded = "true"
+    openMenuBtn.setAttribute("aria-expanded", "true")
+    } else {
+        console.error("an element is missing")
+    }
 }
 
-openMenuBtn.addEventListener("click", openMenu)
+openMenuBtn?.addEventListener("click", openMenu)
 
 // close menu
 function closeMenu() {
+    if (openMenuBtn && closeMenuBtn && navLinks && navOverlay) {
     navLinks.classList.add("hidden")
     closeMenuBtn.classList.add("hidden")
     openMenuBtn.classList.remove("hidden")
     navOverlay.classList.add("hidden")
     navOverlay.classList.add("overlay-hidden")
     navOverlay.classList.remove("overlay-active")
-    openMenuBtn.ariaExpanded = "false"
+    openMenuBtn.setAttribute("aria-expanded", "false")
+    } else {
+        console.error("an element is missing")
+    }
 }
 
-closeMenuBtn.addEventListener("click", closeMenu)
-navOverlay.addEventListener("click", closeMenu)
+closeMenuBtn?.addEventListener("click", closeMenu)
+navOverlay?.addEventListener("click", closeMenu)
